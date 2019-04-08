8a09bb4737a3a0ab639d4981a2fd8d4c225d0f3f
Gustavo Garcia <dannyrr05@gmail.com>
2019-04-08 08:50:32 -0500

    Recorrido matriz en espiral

--- Recorrido Espiral/recorrido.py
+++ Recorrido Espiral/recorrido.py
@@ -3,13 +3,35 @@
 def leerArchivo():
     return[x.split() for x in open("matriz.txt").readlines()]
 
-def leerLinea():
-    print(leerArchivo())
-    print('------------------------')
-    print(leerArchivo()[slice(-1)])
-    print('------------------------')
-    
-    
-leerLinea()    
+def moverDerecha(matriz,x,y,centinela,longitud):
+    if(centinela<longitud):
+        print(matriz[x][y],end=" ")
+        moverDerecha(matriz,x,y+1,centinela+1,longitud)
+    if(centinela==longitud):
+        moverAbajo(matriz,x+1,y-1,0,longitud-1)
+
+def moverAbajo(matriz,x,y,centinela,longitud):
+    if(centinela<longitud):
+        print(matriz[x][y],end=" ")
+        moverAbajo(matriz,x+1,y,centinela+1,longitud)
+    if(centinela==longitud):
+        moverIzquierda(matriz,x-1,y-1,0,longitud)
+
+def moverIzquierda(matriz,x,y,centinela,longitud):
+    if(centinela<longitud):
+        print(matriz[x][y],end=" ")
+        moverIzquierda(matriz,x,y-1,centinela+1,longitud)
+    if(centinela==longitud):
+        moverArriba(matriz,x-1,y+1,0,longitud-1)
+
+def moverArriba(matriz,x,y,centinela,longitud):
+    if(centinela<longitud):
+        print(matriz[x][y],end=" ")
+        moverArriba(matriz,x-1,y,centinela+1,longitud)
+    if(centinela==longitud):
+        moverDerecha(matriz,x+1,y+1,0,longitud)
+
+moverDerecha(leerArchivo(),0,0,0,len(leerArchivo()))
+        
 
     
